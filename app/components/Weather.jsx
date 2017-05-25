b7eadaccc02f95337aa4e34aa813efc8bc1f64c7
Manuel E. Urena <manuel.ureh@hotmail.com>
2017-05-25 00:50:58 -0400

    Fixes issues with weather icon loading

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -7,7 +7,7 @@ import { getDate } from '../helpers/utils';
 const IMAGE_BASE_URL = 'http://res.cloudinary.com/sparklottery/image/upload/v1495686022/';
 
 const Weather = ({ day, onWeatherClick }) => (
-    <div className="col s12 m6 l3" onClick={onWeatherClick} role="button">
+    <div className="col s12 m6 l3 weather-item" onClick={onWeatherClick}>
         <img src={`${IMAGE_BASE_URL}${day.weather[0].icon}.png`} alt="Weather" />
         <div className="center white-text">
             <h4>{getDate(day.dt)}</h4>
--- app/components/WeatherDetails.jsx
+++ app/components/WeatherDetails.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { convertTemp, getDate } from '../helpers/utils';
 
+const IMAGE_BASE_URL = 'http://res.cloudinary.com/sparklottery/image/upload/v1495686022/';
+
 const WeatherDetails = ({ location }) => {
     const weatherData = location.state;
 
@@ -11,7 +13,7 @@ const WeatherDetails = ({ location }) => {
                 <div className="row weather-details center white-text">
                     <div className="col s12">
                         <h1>{location.city}</h1>
-                        <img src={`/app/assets/img/${weatherData.weather[0].icon}.png`} alt="Weather" />
+                        <img src={`${IMAGE_BASE_URL}${weatherData.weather[0].icon}.png`} alt="Weather" />
                         <h2>{getDate(weatherData.dt)}</h2>
                         <h4>{weatherData.weather[0].description}</h4>
                         <h4>Min Temp: {convertTemp(weatherData.temp.min)} ºF</h4>
